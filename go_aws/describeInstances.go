c4c35c3520e828ee5ff7232560cba03713c3ecf8
Pawel Lasak <pawel.lasak@williamhill.com>
2018-05-11 15:47:38 +0200

    Adding

--- go_aws/describeInstances.go
+++ go_aws/describeInstances.go
@@ -3,19 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-func (s session) sess() {
-	ses := s.Must(s.NewSession())
-	ec2Svc := ec2.New(ses)
-	return ec2Svc
-}
-
 func main() {
-	//sess := session.Must(session.NewSession())
-	//ec2Svc := ec2.New(sess)
-	ec2Svc := sess()
+	// Load session from shared config
+	sess := session.Must(session.NewSession())
+	// Create new EC2 client
+	ec2Svc := ec2.New(sess)
 	result, err := ec2Svc.DescribeInstances(nil)
 	if err != nil {
 		fmt.Println("Error", err)
