4f360d852e7be8e6e71d6298640eee4cd2079191
Pablo <pawel22729@gmail.com>
2018-05-28 20:53:34 +0200

    .

--- python_aws/scripts/create_ec2.py
+++ python_aws/scripts/create_ec2.py
@@ -1,7 +1,7 @@
 import boto3
 
 ami = 'ami-cae150b7'
-security=['sg-071de7a8e9c723c3b']
+sg=['sg-071de7a8e9c723c3b']
 ec2 = boto3.resource('ec2')
-inst = ec2.create_instances(ImageId=ami, SecurityGroupIds=, MinCount=1, MaxCount=1, InstanceType='t2.micro')
+inst = ec2.create_instances(ImageId=ami, SecurityGroupIds=sg, MinCount=1, MaxCount=1, InstanceType='t2.micro')
 print(inst[0].id)
