509355ebcc6d2ddfffe64b87c1a0bf9add1d2d88
Pablo <pawel22729@gmail.com>
2018-05-29 21:35:09 +0200

    .

--- python_aws/scripts/create_ec2.py
+++ python_aws/scripts/create_ec2.py
@@ -3,5 +3,6 @@ import boto3
 ami = 'ami-cae150b7'
 sg=['sg-071de7a8e9c723c3b']
 ec2 = boto3.resource('ec2')
-inst = ec2.create_instances(ImageId=ami, SecurityGroupIds=sg, MinCount=1, MaxCount=1, InstanceType='t2.micro')
-print(inst[0].id)
+keyName = u'pls-pair'
+inst = ec2.create_instances(ImageId=ami, SecurityGroupIds=sg, KeyName=keyName, MinCount=1, MaxCount=1, InstanceType='t2.micro')
+print(inst[0].id, inst[0].public_dns_name)
--- python_aws/scripts/list_all_ec2.py
+++ python_aws/scripts/list_all_ec2.py
@@ -4,4 +4,4 @@ ec2 = boto3.resource('ec2')
 all_inst = ec2.instances.all()
 
 for inst in all_inst:
-    print(inst.id, inst.state)
+    print(inst.id, inst.state, inst.public_dns_name)
