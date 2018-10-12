a10bf5738d5b248a966265a94a90a48b7f0ec919
Pawel Lasak <pawel.lasak@williamhill.com>
2018-10-12 13:26:34 +0200

    Adding sample invoke

--- python_aws/scripts/create_ec2.py
+++ python_aws/scripts/create_ec2.py
@@ -7,3 +7,21 @@ ec2 = boto3.resource('ec2')
 keyName = u'pls-pair'
 inst = ec2.create_instances(ImageId=ami, SecurityGroupIds=sg, KeyName=keyName, MinCount=1, MaxCount=1, InstanceType='t2.micro')
 print(inst[0].id, inst[0].public_dns_name)
+
+#rsp = ec2.create_instances(
+#    ImageId='ami-0922553b7b0369273',
+#    InstanceType='t2.micro',
+#    KeyName='pls-lunch-config1',
+#    MaxCount=1,
+#    MinCount=1,
+#    NetworkInterfaces=[
+#        {
+#          'AssociatePublicIpAddress': True,
+#          'DeviceIndex': 0,
+#          'SubnetId': 'subnet-05878964b12833c54',
+#          'Groups': [
+#            'sg-08a1d91877efe7af7'
+#          ]
+#        }
+#    ]
+#)
